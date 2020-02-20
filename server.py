c941fab916ca95759f53491a2c297c147ffe0c7a
kowthalganeshk <kowdal.ganeshkumar@iff.com>
2020-02-20 15:05:05 +0530

    Base commit

--- server.py
+++ server.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+import dialogflow_v2 as dialogflow
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./irbot-rjibay-e94b1844aae0.json";        
 
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
 
-class Employees(Resource):
+class Employee(Resource):
     def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
-    
+        DIALOGFLOW_PROJECT_ID = 'irbot-rjibay'
+        DIALOGFLOW_LANGUAGE_CODE = 'en-US'
+        SESSION_ID = 'kowthalganesh.kowthalraj@gmail.com'
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+        text = 'Hi IRBot!'
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=DIALOGFLOW_LANGUAGE_CODE)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+        return { 'dialog': response.query_result.fulfillment_text }
+
     def post(self):
         conn = db_connect.connect()
         print(request.json)
@@ -40,27 +65,33 @@ class Employees(Resource):
                              Email))
         return {'status':'success'}
 
-    
-class Tracks(Resource):
+
+class Employees(Resource):
     def get(self):
-        conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
-
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from employees") # This line performs query and returns json result
+        req = request.get_json(force=True)        
+        # fetch action from json
+        action = req.get('queryResult').get('action')
+        print(query)
+        resp = { 'dialog': [i[0] for i in query.cursor.fetchall()] }
+        return make_response(jsonify(resp))
     
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
-
-
+    def post(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from employees") # This line performs query and returns json result
+        req = request.get_json(force=True)        
+        # fetch action from json
+        action = req.get('queryResult').get('action')
+        fulfillment_text = ''
+        result = query.cursor.fetchall() 
+        for i in result:
+            fulfillment_text = fulfillment_text +','+ i[1] +'-'+i[3]
+        resp = {'fulfillmentText': fulfillment_text}
+        print(resp)
+        return jsonify(resp)
+    
 api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
-
 
 if __name__ == '__main__':
-     app.run()
+     app.run(port=3000)
